f6dc10ae3371455ede304e122c7a7022cd04f6f0
Eduardo Velho <eduardo.velho@icloud.com>
2022-12-15 19:50:08 -0300

    server session

--- src/auth/getServerSession.ts
+++ src/auth/getServerSession.ts
@@ -4,11 +4,12 @@ import type {
   GetServerSidePropsContext,
 } from "next";
 import { authOptions } from "src/pages/api/auth/[...nextauth]";
-import { unstable_getServerSession } from "next-auth/next";
+import { getSession } from "next-auth/react";
 
 export function getServerSession(
   req: NextApiRequest | GetServerSidePropsContext["req"],
   res: NextApiResponse | GetServerSidePropsContext["res"]
 ) {
-  return unstable_getServerSession(req, res, authOptions);
+  return getSession();
+  //return unstable_getServerSession(req, res, authOptions);
 }
